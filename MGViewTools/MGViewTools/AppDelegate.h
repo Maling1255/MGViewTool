b263e4a00653bb04a585bdde5b77e2780aa5960d
maling <maling@acmeway.com>
2018-04-25 23:35:46 +0800

    初始化数据

---
+++ MGViewTools/MGViewTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MGViewTools
+//
+//  Created by maling  on 2018/4/25.
+//  Copyright © 2018年 maling . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MGViewTools/MGViewTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MGViewTools
+//
+//  Created by maling  on 2018/4/25.
+//  Copyright © 2018年 maling . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
